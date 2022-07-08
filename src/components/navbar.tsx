74f426aa881e2e5da132aaeb84ba5c9c2df66dbc
Zachary Thomas <git@zacharythomas.me>
2022-07-08 21:44:25 +1000

    Add navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./components.css"
+
+export class Navbar extends React.Component{
+  render() {
+    return (
+      <div className="Navbar-header">
+        <h1>Bible FYI</h1>
+      </div>
+    )
+  }
+}
