5f2aa481afb54f4928c2c9513fe4a35bf3c72260
Zachary Thomas <git@zacharythomas.me>
2022-07-17 22:28:12 +1000

    Fixed JSDocs

--- src/views/Home/home.types.ts
+++ src/views/Home/home.types.ts
@@ -1,10 +1,10 @@
 import { BOOKS, VERSIONS } from "../../utils/constants";
 
-/**
- * Error to be used when no such book exists in any copy of the bible.
- * @param {string} book - the string representation of the book.
- */
-export class InvalidBookError extends Error {
+export class InvalidBookError extends Error {  
+  /**
+   * Error to be used when no such book exists in any copy of the bible.
+   * @param {string} book - the string representation of the book.
+   */
   constructor(public book: string) {
     super();
     this.name = "InvalidBookError";
@@ -17,13 +17,13 @@ export class InvalidBookError extends Error {
   }
 }
 
-/**
- * Error to be used when a valid book is submitted, but that book does not exist in the given 
- * version/translation.
- * @param {BOOKS} book - the invalid book in question.
- * @param {VERSIONS} version - the version the book does not exist in
- */
 export class InvalidBookInVersionError extends Error {
+  /**
+   * Error to be used when a valid book is submitted, but that book does not exist in the given 
+   * version/translation.
+   * @param {BOOKS} book - the invalid book in question.
+   * @param {VERSIONS} version - the version the book does not exist in
+   */
   constructor(public book: BOOKS, public version: VERSIONS) {
     super();
     this.name = "InvalidBookInVersionError";
@@ -37,11 +37,11 @@ export class InvalidBookInVersionError extends Error {
   }
 }
 
-/**
- * Error to be used when an invalid reference range is given.
- * @param {string} reference - the invalid reference in question.
- */
 export class InvalidReferenceRangeError extends Error {
+  /**
+   * Error to be used when an invalid reference range is given.
+   * @param {string} reference - the invalid reference in question.
+   */
   constructor(public reference: string) {
     super();
     this.name = 'InvalidReferenceRangeError';
